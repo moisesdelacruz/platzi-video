0c91b5fcc4dd94c9ef7124b921dc4b1ea8a5898f
Moises De La Cruz <moiseslacruz16@gmail.com>
2018-03-16 04:27:29 -0400

    First Commit.
    * Configuracion de Webpack para React.
    * Usando ReactDOM y JSX.
    * Creando Componentes en ReactJS.

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const container = document.getElementById('app');
+
+render(<Media />, container)
---
+++ src/playlist/components/media.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './media.css';
+
+class Media extends Component {
+  render() {
+    return (
+      <div className="Media">
+        <div className="Media-cover">
+          <img
+            src="./images/covers/bitcoin.jpg"
+            alt=""
+            className="Media-image"
+            width={260}
+            height={160}
+          />
+          <h3 className="Media-title">Por qué aprender React?</h3>
+          <p className="Media-author">Moises de la Cruz</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Media;
