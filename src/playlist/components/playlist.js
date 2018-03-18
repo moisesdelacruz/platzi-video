c51348ec6c15f98a185bfb59a41f41e464c0bc1b
Moises De La Cruz <moiseslacruz16@gmail.com>
2018-03-18 01:33:11 -0400

    Composición de componentes.

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Media from './media';
+import Play from '../../icons/components/play';
 
 import './playlist.css';
 
@@ -7,6 +8,7 @@ function Playlist (props) {
   const playlist = props.data.categories[0].playlist;
   return (
     <div className="Playlist">
+      <Play size={50} color='red' />
       {
         playlist.map((item) => {
           return <Media {...item} key={item.id} />
