1913c6480bbb9e34ec4aa4766953473458fe7226
Moises De La Cruz <moiseslacruz16@gmail.com>
2018-03-18 02:15:17 -0400

    Estructura del proyecto con Containers y UI Components.

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Playlist from './src/playlist/components/playlist';
-import data from './src/api.json';
-
-const container = document.getElementById('app');
-
-render(<Playlist data={data} />, container)
---
+++ src/entries/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Home from '../pages/containers/home';
+
+const container = document.getElementById('app');
+
+render(<Home />, container);
