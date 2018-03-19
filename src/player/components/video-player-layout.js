323b35994f1887e3f432c32e1bc127c387e62fa5
Moises De La Cruz <moiseslacruz16@gmail.com>
2018-03-18 23:29:23 -0400

    Iniciando un Reproductor de Vídeo

---
+++ src/player/components/video-player-layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoPlayerLayout = (props) => {
+  return (
+    <div className="VideoPlayer">
+      {props.children}
+    </div>
+  )
+}
+
+export default VideoPlayerLayout;
