17bf050db0c3703f021996a5b76fc1bd50a2f097
Moises De La Cruz <moiseslacruz16@gmail.com>
2018-03-19 02:42:58 -0400

    Duración del video

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -3,10 +3,13 @@ import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
 import PlayPause from '../components/play-pause';
+import Timer from '../components/timer';
+import Controls from '../components/video-player-controls';
 
 class VideoPlayer extends Component {
   state = {
     pause: true,
+    duration: 0,
   }
 
   togglePlay = (event) => {
@@ -21,17 +24,30 @@ class VideoPlayer extends Component {
     })
   }
 
+  handleLoadedMetadata = event => {
+    this.video = event.target;
+    this.setState({
+      duration: this.video.duration,
+    });
+  }
+
   render() {
     return (
       <VideoPlayerLayout>
         <Title title="Esto es un video chido!" />
-        <PlayPause
-          pause={this.state.pause}
-          handleClick={this.togglePlay}
-        />
+        <Controls>
+          <PlayPause
+            pause={this.state.pause}
+            handleClick={this.togglePlay}
+          />
+          <Timer
+            duration={this.state.duration}
+          />
+        </Controls>
         <Video
           autoplay={this.props.autoplay}
           pause={this.state.pause}
+          handleLoadedMetadata={this.handleLoadedMetadata}
           src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
         />
       </VideoPlayerLayout>
