aacf16e3988d1b11e9ed2a7291f1252ef0e059b3
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-06-27 02:52:51 -0500

    Actualizando la aplicación en cada cambio con Subscribe

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const $form = document.getElementById('form');
 
 $form.addEventListener('submit', handleSubmit);
@@ -6,5 +8,56 @@ function handleSubmit (event) {
   event.preventDefault();
   const data = new FormData($form);
   const title = data.get('title');
-  console.log(title);
+  store.dispatch({
+    type: 'ADD_SONG',
+    payload: {
+      title
+    }
+  })
 }
+
+const initialState = [
+  {
+    "title": "Porno"
+  },
+  {
+    "title": "One More time"
+  },
+  {
+    "title": "Cesar"
+  }
+]
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_SONG':
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+function render () {
+  const $container = document.getElementById('playlist');
+  const playlist = store.getState();
+  $container.innerHTML = '';
+  playlist.forEach((item) => {
+    const template = document.createElement('p');
+    template.textContent = item.title;
+    $container.appendChild(template);
+  })
+}
+
+render();
+
+function handleChange () {
+  render();
+}
+
+store.subscribe(handleChange);
