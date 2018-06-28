414ca24d71d861c6f6f0f3eb8292e33bed0dde8d
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-06-28 18:54:27 -0500

    Actualizando un estado inmutable

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -7,6 +7,7 @@ import Modal from '../../widgets/components/modal';
 import HandleError from '../../error/containers/handle-error';
 import VideoPlayer from '../../player/containers/video-player';
 import { connect } from 'react-redux';
+import { List as list } from 'immutable';
 
 
 class Home extends Component {
@@ -62,9 +63,18 @@ function mapStateToProps (state, props) {
     return state.get('data').get('entities').get('categories').get(categoryId)
   });
 
+  let searchResults = list();
+  const search = state.get('data').get('search');
+  if (search) {
+    const mediaList = state.get('data').get('entities').get('media');
+    searchResults = mediaList.filter((item) => (
+      item.get('author').toLowerCase().includes(search.toLowerCase())
+    )).toList()
+  }
+
   return {
     categories,
-    search: state.get('data').get('search')
+    search: searchResults
   }
 }
 
--- src/reducers/data.js
+++ src/reducers/data.js
@@ -5,23 +5,24 @@ import { fromJS } from 'immutable';
 const initialState = fromJS({
   entities: schema.entities,
   categories: schema.result.categories,
-  search: []
+  search: ''
 })
 
 function data (state=initialState, action) {
   switch (action.type) {
     case 'SEARCH_VIDEO': {
-      let results = []
-      if (action.payload.query) {
-        const list = state.data.categories[2].playlist;
-        results = list.filter((item) => {
-          return item.author.toLowerCase().includes(action.payload.query.toLowerCase())
-        })
-      }
-      return {
-        ...state,
-        search: results
-      }
+      // let results = []
+      // if (action.payload.query) {
+      //   const list = state.data.categories[2].playlist;
+      //   results = list.filter((item) => {
+      //     return item.author.toLowerCase().includes(action.payload.query.toLowerCase())
+      //   })
+      // }
+      // return {
+      //   ...state,
+      //   search: results
+      // }
+      return state.set('search', action.payload.query)
     }
     default:
       return state
