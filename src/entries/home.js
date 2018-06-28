590c6f13a428a2dacfb3735a73992e8bf0938534
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-06-27 19:12:04 -0500

    Conectando datos a los Componentes

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Home from '../pages/containers/home';
 // data
 import data from '../api.json';
 
+const initialState = {
+  data: {
+    ...data
+  }
+}
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
+
 const container = document.getElementById('home-container');
 
-render(<Home data={data} />, container);
+
+
+render(
+  <Provider store={store}>
+    <Home />
+  </Provider>, container);
