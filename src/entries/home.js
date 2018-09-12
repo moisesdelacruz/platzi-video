0d89c2220ff168fcb98c0ff5224c7cbb1d7d8304
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-09-12 11:45:22 -0500

    Middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { Map as map } from 'immutable';
 import Home from '../pages/containers/home';
 import reducer from '../reducers';
 
+
+const logger = ({ getState, dispatch }) => next => action => {
+  console.log('old state: ', getState().toJS());
+  console.log('action: ', action);
+  const value = next(action);
+  console.log('new state: ', getState().toJS());
+  return value;
+}
+
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const container = document.getElementById('home-container');
