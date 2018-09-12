e1e933daadc24e40735370968f918d0f464b64a5
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-09-12 12:05:40 -0500

    Añadiendo múltiples Middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -5,21 +5,24 @@ import { Provider } from 'react-redux';
 import { Map as map } from 'immutable';
 import Home from '../pages/containers/home';
 import reducer from '../reducers';
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
-const logger = ({ getState, dispatch }) => next => action => {
-  console.log('old state: ', getState().toJS());
-  console.log('action: ', action);
-  const value = next(action);
-  console.log('new state: ', getState().toJS());
-  return value;
-}
+// const logger = ({ getState, dispatch }) => next => action => {
+//   console.log('old state: ', getState().toJS());
+//   console.log('action: ', action);
+//   const value = next(action);
+//   console.log('new state: ', getState().toJS());
+//   return value;
+// }
 
 const store = createStore(
   reducer,
   map(),
-  applyMiddleware(logger)
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(logger)
+  )
 );
 
 const container = document.getElementById('home-container');
